c0dfbb843573d727e63550765bffdb21495a66b1
piglaker <zxt235813@163.com>
2020-11-24 11:59:48 +0800

    btree

--- synchrome.sh
+++ synchrome.sh
@@ -2,8 +2,7 @@
 
 t=`date`
 
-filelist='synchrome.sh line_table.cpp linked_table.cpp SqStack.cpp SString.cpp BiTree.cpp Graph.cpp line_table.h linked_table.h SqStack.h SString.h BiTree.h Graph.h
- Makefile README.md c_stl/run.sh c_stl/test2.cpp c_stl/demo_if_tree.cpp '
+filelist='line_table.cpp linked_table.cpp SqStack.cpp SString.cpp BiTree.cpp Graph.cpp Sort.cpp line_table.h linked_table.h SqStack.h SString.h BiTree.h Graph.h Sort.h Makefile README.md c_stl/run.sh c_stl/test2.cpp c_stl/demo_if_tree.cpp b-tree.cpp '
 
 echo 'Trans the following Files to docker:ubt_linux && 39.107.229.211 :'
 
@@ -14,31 +13,41 @@ tmp_path="tmp_newlog.log"
 touch $tmp_path
 
 if [[ ! -f "$testFile" ]]; then
-
-echo "create log file：$log_path" >> $tmp_path
-
+echo "Create log file：$log_path" >> $tmp_path
 touch $log_path
-
 fi
 
 echo $t >> $tmp_path
 
+docker_ready=1
+
+if ! docker info >/dev/null 2>&1;then
+	echo "Docker Daemon is not RUNNING ..."
+	docker_ready=0
+else
+	echo "Docker Daemon RUNNING ..."
+fi
+
+
+
 for file in $filelist
 do
-	echo 'please wait...'
+	echo 'Please Wait...'
 	git add $file >> $tmp_path
-	docker cp ./$file ubt_linux:/home/piglaker/c_pck/$file >> $tmp_path
+	if [ $docker_ready == 1 ];then
+		docker cp ./$file ubt_linux:/home/piglaker/c_pck/$file >> $tmp_path
+	fi
 	sshpass -p ZYX1999zyx scp ./$file root@39.107.229.211:/home/zxt/c_prac/$file >> $tmp_path
 	echo $file >> $tmp_path
 done
 
-echo 'push to github' >> $tmp_path
+echo 'Push to github ...' >> $tmp_path
 
 git commit -m "$t" >> $tmp_path
 
 git push -u origin master >> $tmp_path
 
-echo 'done' >> $tmp_path
+echo 'Done !' >> $tmp_path
 
 echo '-----------------------------------------------' >> $tmp_path
 
